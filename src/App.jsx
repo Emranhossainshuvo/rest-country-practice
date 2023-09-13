457378954912ce605b0b0b663c839ba68b56e8de
emran <shuvoorba28@gmail.com>
2023-09-13 06:37:57 +0600

    data consoled from api successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Countries from './components/Countries/Countries'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl font-semibold'>Our world</h1>
+      <h1 className='text-6xl font-semibold'> Lets explore the world</h1>
+      <Countries></Countries>
     </>
   )
 }
